c9d22c7960377f38a2cbba8a73ca680d68520633
Ty Everton <everton_915@yahoo.com>
2021-07-07 17:08:37 -0600

    first

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ app.use(express.static(path.join(__dirname, '../pulic')))
 app.use("styles.css", express.static(path.join(__dirname, '/public/styles.css')))
 
 app.get('/', function(req, res) {
+  
   res.sendFile( path.join(__dirname, '../index.html'))
 
 })
