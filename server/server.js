98e9d94fa597565abb0acf823f6c11f5bf946608
Ty Everton <everton_915@yahoo.com>
2021-07-08 16:55:39 -0600

    final maybe

--- server/server.js
+++ server/server.js
@@ -1,20 +1,20 @@
-const express = require('express')
-const path = require('path')
+// const express = require('express')
+// const path = require('path')
 
-const app = express()
+// const app = express()
 
-app.use(express.static(path.join(__dirname, '../pulic')))
+// app.use(express.static(path.join(__dirname, '../pulic')))
 
-app.use("styles.css", express.static(path.join(__dirname, '/public/styles.css')))
+// app.use("styles.css", express.static(path.join(__dirname, '/styles.css')))
 
-app.get('/', function(req, res) {
+// app.get('/', function(req, res) {
   
-  res.sendFile( path.join(__dirname, '../index.html'))
+//   res.sendFile( path.join(__dirname, '../index.html'))
 
-})
+// })
 
-const port = process.env.port || 4915
+// const port = process.env.port || 4915
 
-app.listen(port, function() {
-  console.log(`Server bumpin on ${port}`)
-})
+// app.listen(port, function() {
+//   console.log(`Server bumpin on ${port}`)
+// })
